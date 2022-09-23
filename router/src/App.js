1542b354dfe5715be7b9e35cfcbf0eaec2478979
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-09-23 16:17:08 +0530

    complete routing in react

--- router/src/App.js
+++ router/src/App.js
@@ -1,10 +1,26 @@
+import Navbar from "./Navbar";
 
-
+import { BrowserRouter as Switch, Route } from "react-router-dom";
+import Home from "./Home";
+import Contact from "./Contact";
+import About from "./About";
 function App() {
   return (
-   <>
-    <h1>hello</h1>
-   </>
+    <>
+      <Navbar />
+
+      <Switch>
+        <Route exact path="/">
+        {  <Home />}
+        </Route>
+        <Route exact path="/about">
+         { <About />}
+        </Route>
+        <Route exact path="/contact">
+          {<Contact />}
+        </Route>
+      </Switch>
+    </>
   );
 }
 
--- router/src/index.js
+++ router/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {BrowserRouter} from 'react-router-dom'
 
+ReactDOM.render(
+  <BrowserRouter>
+  <App/>
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  </BrowserRouter>
+  , document.getElementById("root")
 );
 
 
