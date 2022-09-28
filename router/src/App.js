623cff961176647583a98eec3b0ad3632a644925
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-09-28 23:12:59 +0530

    hurre client side routing done

--- router/src/App.js
+++ router/src/App.js
@@ -1,37 +1,70 @@
-import Navbar from "./Navbar";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-import {BrowserRouter as  Switch, Route } from "react-router-dom";
-import Home from "./Home";
-import Contact from "./Contact";
-import About from "./About";
-import Error from "./Error";
-function App() {
+
+
+export default function BasicExample() {
   return (
-    <>
-      <Navbar />
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
 
-      <Switch>
-      <Route exact path="/"   component={Home}  />
-      <Route exact path="/about"  component={About} />
-      <Route exact path="/contact"  component={Contact} />
-      {/* <Route path='*' component={Error} /> */} 
-    </Switch>
-      {/* <Switch>
-        <Route exact path="/">
-        {  <Home />}
-        </Route>
-        <Route exact path="/about">
-         { <About />}
-        </Route>
-        <Route exact path="/contact">
-          {<Contact />}
-        </Route>
-        <Route component={Error} />
-        only error page not working 
-      </Switch> */}
-      
-    </>
+        <hr />
+
+       
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  );
+}
